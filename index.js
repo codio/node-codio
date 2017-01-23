5c83a06f8a016426088f9f41a17e323e30f61aa8
Dmitrii Suchkov <dsuchkov@codio.com>
2017-01-23 14:31:41 +0400

    Initialize edu organizations with dummy data (#69)
    * Initialize edu organizations with dummy data
    
    Fetch info about dummy data from server
    
    see https://codio.myjetbrains.com/youtrack/issue/codio-8048

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ var BuildManager = require('./lib/build-manager');
 var ImportManager = require('./lib/import-manager');
 var SubscriptionManager = require('./lib/subscription-manager');
 var PlagiarismDetectionManager = require('./lib/plagiarism-detection-manager');
+var EducationTrialManager = require('./lib/trial-manager');
 
 
 module.exports = function (options) {
@@ -41,4 +42,5 @@ module.exports = function (options) {
     this.importManager = new ImportManager(this.options);
     this.subscriptionManager = new SubscriptionManager(this.options);
     this.plagiarismDetectionManager = new PlagiarismDetectionManager(this.options);
+    this.educationTrialManager = new EducationTrialManager(this.options);
 };
